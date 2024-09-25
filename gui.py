ac182b8100cdd9d524d909eec3fb2bde0cd74417
Adnan El-Tayan <aetayann@gmail.com>
2024-09-25 15:32:35 -0600

    implement main frame layout

--- gui.py
+++ gui.py
@@ -1,34 +1,70 @@
 import tkinter as tk
 from tkinter import ttk
-from prayer_times import read_prayer_times
 
-class PrayerTimeGUI:
+class PrayerTimeGUI(tk.Tk):
 
     def __init__(self):
 
-        self.window = tk.Tk()
-        self.window.attributes("-fullscreen", True)
-        self.window.bind('<Escape>', lambda event: self.window.quit())
-        self.prayer_times = read_prayer_times('prayertimes.csv')
+        # main window
+        super().__init__()
+        self.attributes('-fullscreen', True)
+        self.bind('<Escape>', lambda event: self.quit())
 
-        self.frame_layout()
-
-    def frame_layout(self):
-
-        main_top_frame = ttk.Frame(self.window, borderwidth=20, relief= tk.GROOVE)
-        main_top_frame.pack(fill='both', expand = True)
-
-        main_bottom_frame = ttk.Frame(self.window, borderwidth=20, relief= tk.GROOVE)
-        main_bottom_frame.pack(fill='both', expand = True)
-
-
-    def create_widgets(self):
-
-        pass
-
+        # Frames
+        self.main_top_frame = TopFrame(self)
+        self.main_bottom_frame = BottomFrame(self)
 
     def run(self):
 
-        self.window.mainloop()
+        self.mainloop()
+
+class TopFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.place(x=0, y=0, relwidth=1, relheight=0.7)
+        ttk.Label(self, background='green').pack(expand = True, fill = 'both')
+
+class BottomFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.place(x=0, rely=0.7, relwidth=1, relheight=0.3)
+
+        PrayerTimeFrame(self)
+        JummahTimeFrame(self)
+
+class PrayerTimeFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.place(x=0, y=0, relwidth=1, relheight=0.8)
+
+        PrayerTimeEntry(self, 'blue')
+        PrayerTimeEntry(self, 'yellow')
+        PrayerTimeEntry(self, 'blue')
+        PrayerTimeEntry(self, 'yellow')
+        PrayerTimeEntry(self, 'blue')
+
+
+class JummahTimeFrame(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.place(x=0, rely=0.8, relwidth=1, relheight=0.2)
+        JummahTimeEntry(self)
+
+
+class PrayerTimeEntry(ttk.Frame):
+    def __init__(self, parent, background):
+        super().__init__(parent)
+        ttk.Label(self, background=background).pack(expand = True, fill = 'both')
+        self.pack(side='left', expand = True, fill = 'both')
+
+class JummahTimeEntry(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        ttk.Label(self, background='orange').pack(expand = True, fill = 'both')
+        self.pack(side='bottom', expand = True, fill = 'both')
+
+
+
+PrayerTimeGUI()
 
 
